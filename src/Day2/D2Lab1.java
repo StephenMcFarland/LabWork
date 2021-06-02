8df48db3211f84b008bd96c73f410e831cca32c5
stephenmcfarland <snmcfarland@gmail.com>
2021-06-02 16:03:16 -0400

    Day 2 Lab1

---
+++ src/Day2/D2Lab1.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class D2Lab1 {
+
+	public static void main(String[] args) {
+		for(int i = 1;i<10;i++) {
+		for(int j = 1;j<10;j++) {
+			int product = i*j;
+			System.out.print(product+" ");
+		}
+		System.out.println("");
+	}
+
+	}
+
+}
