5219cf46dcd61a8ecf294f71aca1c43454a93785
stephenmcfarland <snmcfarland@gmail.com>
2021-06-02 16:14:06 -0400

    Day 2 Lab3

---
+++ src/Day2/D2Lab3.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class D2Lab3 {
+
+	public static void main(String[] args) {
+		// Display lowercase alphabet using for loop HINT -  for(c = 'a'; c <= 'z'; ++c)
+		char c;
+		
+		for(c= 'a'; c<='z';++c) {
+			System.out.print(c+"  ");
+		}
+		
+	}
+
+}
