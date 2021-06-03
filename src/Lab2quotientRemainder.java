b6c6473899d62e0f5cab2416217fe60baecca57f
stephenmcfarland <snmcfarland@gmail.com>
2021-06-03 09:05:20 -0400

    Day 1 Lab2

---
+++ src/Lab2quotientRemainder.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Lab2quotientRemainder {
+	
+	static Scanner s = new Scanner(System.in);
+	
+	
+	static int num1;
+	static int num2;
+	
+	public static void main(String[] args) {
+		// Compute Quotient and Remainder for provided number.
+		
+		
+		
+			
+			
+			System.out.println("Enter the first number:  ");
+			num1 = s.nextInt();
+			
+			System.out.println("Enter the second number:  ");
+			num2 = s.nextInt();
+			
+			System.out.println("\nThe quotient is:  "+num1/num2);
+			
+			
+			System.out.println("\nThe remainder is:  "+num1%num2);
+					}
+
+	
+
+}
