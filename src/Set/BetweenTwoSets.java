3c5b12ecf1714e800c52e608bd2fe33259f314c3
Stephen McFarland <snmcfarland@gmail.com>
2022-07-09 15:39:13 -0400

    between 2 sets

--- src/Set/BetweenTwoSets.java
+++ src/Set/BetweenTwoSets.java
@@ -1,11 +1,19 @@
+package Set;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class BetweenTwoSets {
 
 	public static void main(String[] args) {
 		// 
-		//ArrayList<Integer> fa = new ArrayList<Integer>();
+		
+		ArrayList<Integer> a = new ArrayList<>(Arrays.asList(2,6));
+		ArrayList<Integer> b = new ArrayList<>(Arrays.asList(24,36));
 	    HashSet<Integer> f = new HashSet<Integer>();
+	    
+	    /*
 	    boolean factor = false;
 	    
 	    for(int ba=1;ba<101;ba++){
@@ -36,9 +44,11 @@ public class BetweenTwoSets {
 	            }
 	        }
 	    }
-	    return f.size();
+	    */
+	     System.out.println("test");
+	    //System.out.println(f.size());
+	   // System.out.println(f);
 	    }
 
 	}
 
-}
